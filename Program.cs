e96eb6233646bc7b70e6e9445da1acbb9406af84
jose-garcia-jdas <jose.garcia@blueyonder.com>
2020-11-11 11:10:23 -0600

    first commit.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Components;
+using System;
+
+namespace nes_emu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            SayHello(i++);
+            SayHello(i++);
+            SayHello(i++);
+            SayHello(i++);
+            SayHello(i++);
+            SayHello(i++);
+            SayHello(i++);
+
+            Bus bus = new Bus();
+
+            while(bus.Cpu.Cycles > 0 || Console.ReadKey().Key == ConsoleKey.Spacebar)
+            {
+                bus.Cpu.Clock();
+            }
+        }
+
+        internal static void SayHello(int i)
+        {
+            Console.WriteLine($"Hello World! {i}");
+        }
+    }
+}
